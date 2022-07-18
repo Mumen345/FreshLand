d73408dce0719222e7d332dc252c3ba62c5e2283
Mumen Elijah <kingxosas@gmail.com>
2022-07-18 12:31:40 +0100

    everything done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 // import logo from "logo.png";
 import "./App.css";
-
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Fresh Land</title>
+        <link rel="Fresh land" href="http://www.freshlandfood.com" />
+        <meta
+          name="description"
+          content="Get your Dried catfish at a cheap an affordable price"
+        />
+      </Helmet>
       <img className="App-logo " src="logo.png" alt="logo" />
       <h1>COMING SOON</h1>
       <p>
